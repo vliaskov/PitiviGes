ce752cfee473559a9ad4b3de0922ed9b2c1f46ea
Mathieu Duponchelle <mathieu.duponchelle@epitech.eu>
2013-07-16 19:42:53 +0200

    tests/integration: adds image_filename in the test generation macro

--- tests/check/ges/integration.c
+++ tests/check/ges/integration.c
@@ -581,17 +581,17 @@ test_image (void)
 #define CREATE_TEST(name, func, profile)                                       \
 GST_START_TEST (test_##name##_raw_h264_mov)                                    \
 {                                                                              \
-  testfilename1 = "raw_h264.0.mov";                                            \
-  testfilename2 = "raw_h264.1.mov";                                            \
-  current_profile = profile;                                                   \
+  testfilename1 = "test1.MOV";                                                 \
+  testfilename2 = "test2.MOV";                                                 \
+  test_image_filename = "test.png";      				       \
   func ();                                                                     \
 }                                                                              \
 GST_END_TEST;                                                                  \
 GST_START_TEST (test_##name##_vorbis_theora_ogv)                               \
 {                                                                              \
-  testfilename1 = "vorbis_theora.0.ogg";                                       \
-  testfilename2 = "vorbis_theora.1.ogg";                                       \
-  current_profile = profile;                                                   \
+  testfilename1 = "test1.ogv";                                                 \
+  testfilename2 = "test2.ogv";                                                 \
+  test_image_filename = "test.png";                                            \
   func ();                                                                     \
 }                                                                              \
 GST_END_TEST;                                                                  \
@@ -599,6 +599,7 @@ GST_START_TEST (test_##name##_vorbis_vp8_webm)                                 \
 {                                                                              \
   testfilename1 = "vorbis_vp8.0.webm";                                         \
   testfilename2 = "vorbis_vp8.1.webm";                                         \
+  test_image_filename = "test.png";					       \
   current_profile = profile;                                                   \
   func ();                                                                     \
 }                                                                              \
@@ -608,6 +609,7 @@ GST_START_TEST (test_##name##_mp3_h264_mov)                                    \
   testfilename1 = "mp3_h264.0.mov";                                            \
   testfilename2 = "mp3_h264.1.mov";                                            \
   current_profile = profile;                                                   \
+  test_image_filename = "test.png";					       \
   func ();                                                                     \
 }                                                                              \
 GST_END_TEST;
